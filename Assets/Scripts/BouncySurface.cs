9420651a6b9d268be76c12b8869556d062daff65
Rivers Jackson <jacksonrivers02@gmail.com>
2024-09-21 16:25:47 -0600

    added sound effects for collisions and points scored

--- Assets/Scripts/BouncySurface.cs
+++ Assets/Scripts/BouncySurface.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BouncySurface : MonoBehaviour
 {
     public float bounceStrength;
+    public AudioClip bounceClip;
 
     private void OnCollisionEnter2D(Collision2D collision){
         Ball ball = collision.gameObject.GetComponent<Ball>();
@@ -12,6 +13,7 @@ public class BouncySurface : MonoBehaviour
         if (ball != null){
             Vector2 normal = collision.GetContact(0).normal;
             ball.AddForce(-normal * this.bounceStrength);
+            SoundFXManager.instance.PlaySoundFXClip(bounceClip, transform, 1.0f);
         }
     }
 }
--- Assets/Scripts/ScoringZone.cs
+++ Assets/Scripts/ScoringZone.cs
@@ -6,12 +6,14 @@ using UnityEngine.EventSystems;
 public class ScoringZone : MonoBehaviour
 {
     public EventTrigger.TriggerEvent scoreTrigger;
+    public AudioClip bounceClip;
     private void OnCollisionEnter2D(Collision2D collision){
         Ball ball = collision.gameObject.GetComponent<Ball>();
 
         if (ball != null){
             BaseEventData eventData = new BaseEventData(EventSystem.current);
             this.scoreTrigger.Invoke(eventData);
+            SoundFXManager.instance.PlaySoundFXClip(bounceClip, transform, 1.0f);
         }
     }
 }
